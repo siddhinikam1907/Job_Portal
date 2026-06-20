8dad048b4e883424b9c5fef9d401e0d458151e03
Siddhi Dhairyashil Nikam <siddhinikam1907@gmail.com>
2026-06-20 15:37:51 +0530

    Fix CORS for Render frontend

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); //req.cookies
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://job-portal-1-93ed.onrender.com"],
   credentials: true,
 };
 
